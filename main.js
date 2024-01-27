d4d2087e231ca5cacf3b633e800d8b7d8e28c210
patchy <112994743+patrickptasznik@users.noreply.github.com>
2024-01-27 01:53:23 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,94 @@
+//hiding header
+let prevScrollPos = window.scrollY;
+
+window.onscroll = function() {
+    const currentScrollPos = window.scrollY;
+
+    if (prevScrollPos > currentScrollPos) {
+        document.querySelector('header').style.top = '0';
+    } else {
+        document.querySelector('header').style.top = '-8rem';
+    }
+
+    prevScrollPos = currentScrollPos;
+};
+
+//responsive nav links on taskbar as you scroll
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".navbar a");
+
+    window.addEventListener("scroll", () => {
+        let currentSection = "";
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (scrollY >= sectionTop - sectionHeight / 3) {
+                currentSection = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach((link) => {
+            link.classList.remove("active");
+            if (link.getAttribute("href").substring(1) === currentSection) {
+                link.classList.add("active");
+            }
+        });
+    });
+});
+
+//footer scroll-up button
+var scrollToTopBtn = document.querySelector(".footer-scroll-up a");
+
+  scrollToTopBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+});
+
+//alternating rotate for about images
+document.querySelectorAll('.about-img').forEach(function(element) {
+    element.addEventListener('mouseenter', function() {
+        if (element.classList.contains('rotate-left')) {
+            element.classList.remove('rotate-left');
+            element.classList.add('rotate-right');
+        } else {
+            element.classList.remove('rotate-right');
+            element.classList.add('rotate-left');
+        }
+    });
+});
+
+//alternating rotate for logo
+document.querySelector('.name').addEventListener('mouseenter', function() {
+    const nameElement = this;
+    if (nameElement.classList.contains('rotate-left')) {
+        nameElement.classList.remove('rotate-left');
+        nameElement.classList.add('rotate-right');
+    } else {
+        nameElement.classList.remove('rotate-right');
+        nameElement.classList.add('rotate-left');
+    }
+});
+
+//alternating backgrounds
+document.addEventListener('DOMContentLoaded', function () {
+    const backgrounds = ['url(images/background.png)', 'url(images/backgroundbw.png)'];
+    const homeSection = document.querySelector('.home');
+    let currentBackgroundIndex = 0;
+  
+    function changeBackground() {
+      homeSection.style.backgroundImage = backgrounds[currentBackgroundIndex];
+      currentBackgroundIndex = (currentBackgroundIndex + 1) % backgrounds.length;
+  
+      const interval = currentBackgroundIndex === 0 ? 2000 : 5000;  //time in ms
+  
+      setTimeout(changeBackground, interval);
+    }
+
+    changeBackground();
+  });
